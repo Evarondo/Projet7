51a241ef55686a9a9348b33ee17bb07f5be52159
Evarondo <eva-rondeau@orange.fr>
2023-07-04 16:54:03 +0200

    api

--- api_2.py
+++ api_2.py
@@ -219,6 +219,5 @@ async def get_features(client_id: int):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
