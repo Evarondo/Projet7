77aa74fedfdbdfb2ae8a007440d6e297bb7a34e6
Evarondo <eva-rondeau@orange.fr>
2023-07-06 15:44:15 +0200

    api

--- api.py
+++ api.py
@@ -68,7 +68,7 @@ async def get_threshold():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8003))
-    uvicorn.run(api:app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 # In[ ]:
